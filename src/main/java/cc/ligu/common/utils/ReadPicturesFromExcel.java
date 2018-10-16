94bafa4ba078bcef8ca70d23be94905e1874e70c
zhangjianyu <admin@example.com>
2018-10-16 14:49:36 +0800

    sss

--- src/main/java/cc/ligu/common/utils/ReadPicturesFromExcel.java
+++ src/main/java/cc/ligu/common/utils/ReadPicturesFromExcel.java
@@ -1,8 +1,8 @@
-package cc.gukeer.common;
+package cc.ligu.common.utils;
 
+import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.PictureData;
-import org.apache.poi.xssf.usermodel.*;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -15,23 +15,23 @@ public class ReadPicturesFromExcel {
             Exception {
 
         InputStream inp = new FileInputStream(
-                "C:\\Users\\shenyy\\Desktop\\壁纸\\Fw_ 合作单位施工人员档案（武汉贝斯特）18.10.11\\合作单位施工人员档案（中通一局）18.10.9.xlsx");
-        XSSFWorkbook workbook = new XSSFWorkbook(inp);
+                "C:\\Users\\shenyy\\Desktop\\壁纸\\444\\111.xls");
+        HSSFWorkbook workbook = new HSSFWorkbook(inp);
 
-        List<XSSFPictureData> pictures = workbook.getAllPictures();
-        XSSFSheet sheet = (XSSFSheet) workbook.getSheetAt(0);
+        List<HSSFPictureData> pictures = workbook.getAllPictures();
+        HSSFSheet sheet = (HSSFSheet) workbook.getSheetAt(0);
 
         int i = 0;
-        for (XSSFShape shape : sheet.getDrawingPatriarch().getShapes()) {
-            XSSFClientAnchor anchor = (XSSFClientAnchor) shape.getAnchor();
+        for (HSSFShape shape : sheet.getDrawingPatriarch().getChildren()) {
+            HSSFClientAnchor anchor = (HSSFClientAnchor) shape.getAnchor();
 
-            if (shape instanceof XSSFPicture) {
-                XSSFPicture pic = (XSSFPicture) shape;
+            if (shape instanceof HSSFPicture) {
+                HSSFPicture pic = (HSSFPicture) shape;
                 int row = anchor.getRow1();
                 System.out.println(i + "--->" + anchor.getRow1() + ":"
                         + anchor.getCol1());
-                int pictureIndex = i;//pic.getPictureIndex() - 1;
-                XSSFPictureData picData = pictures.get(pictureIndex);
+                int pictureIndex = pic.getPictureIndex() - 1;
+                HSSFPictureData picData = pictures.get(pictureIndex);
 
                 System.out.println(i + "--->" + pictureIndex);
                 savePic(sheet, row, picData);
@@ -40,33 +40,20 @@ public class ReadPicturesFromExcel {
         }
     }
 
-    private static void savePic(XSSFSheet sheet, int i, PictureData pic) throws Exception {
+    private static void savePic(HSSFSheet sheet, int i, PictureData pic) throws Exception {
 
-        XSSFRow s = sheet.getRow(i);
-        XSSFCell d = s.getCell(5);
+        HSSFRow s = sheet.getRow(i);
+        HSSFCell d = s.getCell(0);
         String name = d.getStringCellValue();
 
         String ext = pic.suggestFileExtension();
 
         byte[] data = pic.getData();
-        if (ext.equals("jpeg")) {
-            FileOutputStream out = new FileOutputStream(
-                    "C:\\Users\\shenyy\\Desktop\\壁纸\\头像\\" + name + ".jpg");
-            out.write(data);
-            out.close();
-        }
-        if (ext.equals("dib")) {
-            FileOutputStream out = new FileOutputStream(
-                    "C:\\Users\\shenyy\\Desktop\\壁纸\\头像\\" + name + ".bmp");
-            out.write(data);
-            out.close();
-        }
-        if (ext.equals("png")) {
-            FileOutputStream out = new FileOutputStream(
-                    "C:\\Users\\shenyy\\Desktop\\壁纸\\头像\\" + name + ".png");
-            out.write(data);
-            out.close();
-        }
+
+        FileOutputStream out = new FileOutputStream(
+                "C:\\Users\\shenyy\\Desktop\\壁纸\\头像\\" + name + ".jpg");
+        out.write(data);
+        out.close();
     }
 
 }
