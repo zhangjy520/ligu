677d19781936bf3c6213696f0d3c745e7eb6b824
zjy@ty <1211079133@qq.com>
2019-06-18 13:48:30 +0800

    压缩图片

--- src/main/java/cc/ligu/common/Test.java
+++ src/main/java/cc/ligu/common/Test.java
@@ -1,50 +1,77 @@
 package cc.ligu.common;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import net.sf.json.JSONObject;
 
-
-import java.util.*;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Test {
-    public void test(String t){
-        System.out.println(t+"秒后调用了test:");
-    }
-
-    public void test1(String t, ArrayList<String> tes){
-        System.out.println(t+"秒后调用了test1:list的第一个参数是"+tes.get(0));
-    }
-
-    public static List<Map> list = new ArrayList<>();
-    public static void generateJson(){
-        StringBuffer sb = new StringBuffer();
-        for (int j = 0; j < 2000; j++) {
-            int startIndex = j*8;
-            for (int i = 0; i < 8; i++) {
-                if (i==0){
-                    String aa ="{id:"+(i+startIndex)+",pId:"+0+",name:\"test"+i+"\", open:true},";
-                    sb.append(aa);
-                }else{
-                    String aa ="{id:"+(i+startIndex)+",pId:"+(i+startIndex-1)+",name:\"test"+i+"\", open:true},";
-                    sb.append(aa);
-                }
-            }
-        }
-
-        System.out.println(sb.toString());
-
-    }
-
     public static void main(String[] args) {
-        generateJson();
-
-        System.out.println(list);
-
-        String aad = "{分管领导:'hjjhjjj',主任:'hjj',管理员:'jjj'}";
-        JSONObject res = JSONObject.fromObject(aad);
-//        JSONObject res1 = JSONObject.fromObject("{分管领导:'hjjhjjj',主任:'hjj',管理员:'jjj}");
-        System.out.println(res.toString());
+        try {
+            compressPic("C:\\Users\\dell\\Desktop\\大图\\1536304794566.jpg","C:\\Users\\dell\\Desktop\\大图\\1536304794566_small.jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    static Map<Object,Map<Object,Map<Object,Map<Object,List<Map<String,String>>>>>> m = new HashMap();
+    public static boolean compressPic(String srcFilePath, String descFilePath) throws IOException {
+        File file = null;
+        BufferedImage src = null;
+        FileOutputStream out = null;
+        ImageWriter imgWrier;
+        ImageWriteParam imgWriteParams;
+
+        // 指定写图片的方式为 jpg
+        imgWrier = ImageIO.getImageWritersByFormatName("jpg").next();
+        imgWriteParams = new javax.imageio.plugins.jpeg.JPEGImageWriteParam(
+                null);
+        // 要使用压缩，必须指定压缩方式为MODE_EXPLICIT
+        imgWriteParams.setCompressionMode(imgWriteParams.MODE_EXPLICIT);
+        // 这里指定压缩的程度，参数qality是取值0~1范围内，
+        imgWriteParams.setCompressionQuality((float)0.1);
+        imgWriteParams.setProgressiveMode(imgWriteParams.MODE_DISABLED);
+        ColorModel colorModel = ImageIO.read(new File(srcFilePath)).getColorModel();// ColorModel.getRGBdefault();
+        // 指定压缩时使用的色彩模式
+//        imgWriteParams.setDestinationType(new javax.imageio.ImageTypeSpecifier(
+//                colorModel, colorModel.createCompatibleSampleModel(16, 16)));
+        imgWriteParams.setDestinationType(new javax.imageio.ImageTypeSpecifier(
+                colorModel, colorModel.createCompatibleSampleModel(16, 16)));
+
+        try {
+            if (isBlank(srcFilePath)) {
+                return false;
+            } else {
+                file = new File(srcFilePath);
+                System.out.println(file.length());
+                src = ImageIO.read(file);
+                out = new FileOutputStream(descFilePath);
+
+                imgWrier.reset();
+                // 必须先指定 out值，才能调用write方法, ImageOutputStream可以通过任何
+                // OutputStream构造
+                imgWrier.setOutput(ImageIO.createImageOutputStream(out));
+                // 调用write方法，就可以向输入流写图片
+                imgWrier.write(null, new IIOImage(src, null, null),
+                        imgWriteParams);
+                out.flush();
+                out.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isBlank(String string) {
+        if (string == null || string.length() == 0 || string.trim().equals("")) {
+            return true;
+        }
+        return false;
+    }
 }
