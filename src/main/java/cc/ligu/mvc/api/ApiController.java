d2e71e37683842197cc8fdd8e938666fa8965b2d
jyzhang29 <jyzhang29@iflytek.com>
2021-01-11 17:09:47 +0800

    fasdasdas

--- src/main/java/cc/ligu/mvc/api/ApiController.java
+++ src/main/java/cc/ligu/mvc/api/ApiController.java
@@ -3,11 +3,14 @@
  */
 package cc.ligu.mvc.api;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wordnik.swagger.annotations.ApiImplicitParam;
 import com.wordnik.swagger.annotations.ApiImplicitParams;
 import com.wordnik.swagger.annotations.ApiOperation;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -19,6 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import cc.ligu.common.controller.BasicController;
 import cc.ligu.common.entity.ResultEntity;
+import cc.ligu.mvc.persistence.entity.AppConfig;
 import cc.ligu.mvc.service.MoneyService;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
@@ -62,15 +66,10 @@ public class ApiController extends BasicController {
         return ResultEntity.newResultEntity(res);
     }
 
-    @ApiOperation(value = "test", httpMethod = "POST", notes = "")
-    @ApiImplicitParams({
-        @ApiImplicitParam(paramType = "query", dataType = "String", name = "nameList", value = "基金名称集合逗号隔开", required = true),
-        @ApiImplicitParam(paramType = "query", dataType = "String", name = "numList", value = "基金金额集合逗号隔开", required = true),})
-    @RequestMapping("/queryMoney1")
-    public ResultEntity queryMoney1(HttpServletRequest request) {
-        String nameList = request.getParameter("nameList");
-
-        return ResultEntity.newResultEntity(nameList);
+    @ResponseBody
+    @RequestMapping(value = "/queryMoney1", method = RequestMethod.POST)
+    public ResultEntity queryMoney1(HttpServletRequest request, AppConfig appConfig) {
+        return ResultEntity.newResultEntity(appConfig);
     }
 
     @ApiOperation(value = "通过客户端id判断是否需要登录", httpMethod = "POST", notes = "验证是否需要登录,不需要登录返回用户信息")
